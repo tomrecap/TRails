87c4fe53897f43c3b7410f54d1cf49ff264889d8
Thomas Carroll <thomas.carroll08@gmail.com>
2014-03-27 11:54:12 -0400

    Removes superfluous comments

--- lib/rails_lite/router.rb
+++ lib/rails_lite/router.rb
@@ -10,7 +10,6 @@ class Route
     @action_name = action_name
   end
 
-  # checks if pattern matches path and method matches request method
   def matches?(req)
     @http_method.downcase == req.request_method.downcase &&
     @pattern.match(req.path)
@@ -24,11 +23,8 @@ class Route
     route_params
   end
 
-  # use pattern to pull out route params (save for later?)
-  # instantiate controller and call controller action
   def run(req, res)
     route_params = extract_route_params(req)
-    # debugger
     controller = @controller_class.new(req, res, route_params)
 
     controller.invoke_action(@action_name)
@@ -42,20 +38,15 @@ class Router
     @routes = []
   end
 
-  # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
     @routes << Route.new(pattern, method,
       controller_class, action_name)
   end
 
-  # evaluate the proc in the context of the instance
-  # for syntactic sugar :)
   def draw(&proc)
     self.instance_eval(&proc)
   end
 
-  # make each of these methods that
-  # when called add route
   [:get, :post, :put, :delete].each do |http_method|
     define_method(http_method) do |pattern, controller_class, action|
       add_route(pattern, http_method.to_sym, controller_class, action)
@@ -70,10 +61,8 @@ class Router
     end
   end
 
-  # either throw 404 or call run on a matched route
   def run(req, res)
     route = match(req)
-    # debugger
     route.nil? ? res.status = 404 : route.run(req, res)
   end
 end
