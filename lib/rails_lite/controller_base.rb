fece031deca9782d6d2e0f0b3c84dc918ddcf8e7
Thomas Carroll <thomas.carroll08@gmail.com>
2014-03-27 11:52:12 -0400

    Removes superfluous comments

--- lib/rails_lite/controller_base.rb
+++ lib/rails_lite/controller_base.rb
@@ -10,7 +10,6 @@ class ControllerBase
 
   @@form_authenticity_tokens ||= []
 
-  # setup the controller
   def initialize(req, res, route_params = {})
     @req, @res = req, res
     @already_rendered = false
@@ -18,7 +17,6 @@ class ControllerBase
     @params = Params.new(req, route_params)
 
     update_form_authenticity_tokens_array
-    # @@form_authenticity_token = SecureRandom.urlsafe_base64(16)
   end
 
   def update_form_authenticity_tokens_array
@@ -28,9 +26,6 @@ class ControllerBase
     @@form_authenticity_tokens
   end
 
-  # populate the response with content
-  # set the responses content type to the given type
-  # later raise an error if the developer tries to double render
   def render_content(content, type)
     raise "error" if already_rendered?
     @already_rendered = true
@@ -60,13 +55,6 @@ class ControllerBase
     session.store_session(@res)
   end
 
-  # def render_content(content, type)
-  # end
-
-  # def form_authenticity_token
-  #   @@form_authenticity_token
-  # end
-
   def form_authenticity_tokens
     @@form_authenticity_tokens
   end
@@ -78,8 +66,6 @@ class ControllerBase
       .include?(params[:authenticity_token])
   end
 
-  # use ERB and binding to evaluate templates
-  # pass the rendered html to render_content
   def render(template_name)
     path =
       "views/#{self.class.to_s.underscore}/#{template_name}.html.erb"
@@ -89,7 +75,6 @@ class ControllerBase
     render_content(erb_result, "text/html")
   end
 
-  # method exposing a `Session` object
   def session
     @session ||= Session.new(@req)
   end
@@ -98,14 +83,8 @@ class ControllerBase
     @flash ||= Flash.new(@req)
   end
 
-  # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
-    # unless authenticity_token_is_valid?
-    #   raise "authenticity token is missing or invalid."
-    # end
-
     self.send(name)
-
     render(name) unless already_rendered?
   end
 end
